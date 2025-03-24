d02930dc3e639ce6959e92dd2f8cffad62d25f8b
rickdohmen <rickdohmen2003@gmail.com>
2025-03-24 18:06:06 +0100

    added menu controller

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenu : MonoBehaviour
+public class MenuController : MonoBehaviour
 {
-    [SerializeField] private GameObject mainMenuPrefab;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject settingsMenu;
+    [SerializeField] private GameObject creditsMenu;
+
+    
 
     // Start is called before the first frame update
     void Start()
