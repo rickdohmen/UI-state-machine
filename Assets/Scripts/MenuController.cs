cf48be6976155d7c18439b076c4ea08c8d21672d
rickdohmen <rickdohmen2003@gmail.com>
2025-03-25 20:24:00 +0100

    added a public enum and a better menucontroller script

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public abstract class MenuController : MonoBehaviour
+public static class MenuController
 {
-    public void StartGame()
+    public static bool isInitialized { get; private set; }
+    public static GameObject mainMenu, settingsMenu, creditsMenu;
+    
+    public static void Initialize()
     {
-        SceneManager.LoadScene("Game");
+        if (isInitialized) return;
+        mainMenu = GameObject.Find("MainMenu");
+        settingsMenu = GameObject.Find("SettingsMenu");
+        creditsMenu = GameObject.Find("CreditsMenu");
+        isInitialized = true;
     }
-
-    public void Credits() => SceneManager.LoadScene("Credits");
-
-    public void Settings() => SceneManager.LoadScene("Settings");
-
-    public void QuitGame() => Application.Quit();
+    
+    public static void LoadMenu(Menu menu, GameObject callingMenu)
+    {
+        switch (menu)
+        {
+            case Menu.MAIN_MENU:
+                mainMenu.SetActive(true);
+                settingsMenu.SetActive(false);
+                creditsMenu.SetActive(false);
+                break;
+            case Menu.SETTINGS:
+                mainMenu.SetActive(false);
+                settingsMenu.SetActive(true);
+                creditsMenu.SetActive(false);
+                break;
+            case Menu.CREDITS:
+                mainMenu.SetActive(false);
+                settingsMenu.SetActive(false);
+                creditsMenu.SetActive(true);
+                break;
+        }
+    }
 }
